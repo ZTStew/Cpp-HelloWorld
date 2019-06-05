facf37b2d81a6cfae6b53c9ea116b7157c2e61d2
[Zach.S] <[ZT.Stewart.5@gmail.com]>
2019-06-05 11:00:56 -0400

    First C++ program

---
+++ helloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/**
+First C++ program created, Hello World
+
+Started: 6/3/2019
+Finished: 6/3/2019
+*/
+
+int main(){
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
